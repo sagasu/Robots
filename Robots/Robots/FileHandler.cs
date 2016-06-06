99a7a045d74680d0fc9f29af58953069371ab2a8
Mateusz Kopij <sagasu@gmail.com>
2016-06-06 17:08:42 +0100

    Implementing a simple handler to read content from files.

---
+++ Robots/Robots/FileHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace RobotsTests
+{
+    public class FileHandler
+    {
+        public IEnumerable<string> GetAllLines(string filePath = "robotCommands.txt")
+        {
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
---
+++ Robots/RobotsTests/FileHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RobotsTests
+{
+    [TestFixture]
+    class FileHandlerTests
+    {
+        [Test]
+        public void GetAllLines_WhenCalledWithoutFilePath_ReadsAllLinesFromDefaultFileLocation()
+        {
+            var fh = new FileHandler();
+            var lines = fh.GetAllLines();
+
+            Assert.AreEqual("00 11", lines.First());
+        }
+
+        [Test]
+        public void GetAllLines_WhenCalledWithCustomFilePath_ReadsAllLinesFromSelectedFile()
+        {
+            var fh = new FileHandler();
+            var lines = fh.GetAllLines("customRobotCommands.txt");
+
+            Assert.AreEqual("12 34", lines.First());
+        }
+    }
+}
