628664e1b599382bd538cbb38b6e12cd34a57cd9
sagasu <sagasu@gmail.com>
2016-06-06 20:57:31 +0100

    Introduces InputManager that will perform all the commands based on file or an passed list of commands - that is easier to use for testing. This is initial commit of this class, there is a huge space for improvement of a code base here - moving certain logic to separate classes, intruducing validation. It is more a first draft to check if it will work, and to start adding additional harnes and applying refactoring on top of it.

---
+++ Robots/Robots/InputManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Robots
+{
+    public class InputManager
+    {
+        private readonly IDictionary<string, Expression<Action>> _validOperations = new Dictionary
+            <string, Expression<Action>>();
+
+        private Simulator _simulator;
+
+        public InputManager()
+        {
+            // closure on purpose 
+            _validOperations.Add("F", () => _simulator.Forward());
+            _validOperations.Add("L", () => _simulator.Left());
+            _validOperations.Add("R", () => _simulator.Right());
+        }
+
+        public void Run(string path)
+        {
+            Run(new FileHandler().GetAllLines(path));
+        }
+
+        public void Run(IList<string> getAllLines)
+        {
+            var board = BuildBoard(getAllLines.First());
+            var isRobotPositionCommand = true;
+
+            foreach (var command in getAllLines.Skip(1))
+            {
+                if (isRobotPositionCommand)
+                {
+                    var robot = BuildRobot(command);
+                    if (_simulator == null)
+                    {
+                        _simulator = new Simulator(board, robot);
+                    }
+                    else
+                    {
+                        _simulator.SetRobot(robot);
+                    }
+                    
+                    isRobotPositionCommand = false;
+                    continue;
+                }
+
+                MoveRobot(command);
+
+            }
+        }
+
+        public string Report()
+        {
+            return _simulator.Report();
+        }
+
+        private void MoveRobot(string commands)
+        {
+            foreach (var command in commands)
+            {
+                _validOperations[command.ToString()].Compile().Invoke();
+            }
+        }
+
+
+        private Robot BuildRobot(string command)
+        {
+            var robotParameters = command.Split(' ');
+            var positionX = int.Parse(robotParameters[0]);
+            var positionY = int.Parse(robotParameters[1]);
+            var orientation = robotParameters[2];
+
+            var cardinalEngine = Cardinal.GetAllCardinalEngines().FirstOrDefault(carEngine => carEngine.CardinalDirection == orientation);
+
+            if (cardinalEngine == null)
+            {
+                Console.WriteLine("Incorrect robot position command it should have following format: 1 2 N");
+            }
+
+            return new Robot(new Position(positionX, positionY), cardinalEngine);
+        }
+
+        private Board BuildBoard(string command)
+        {
+            var boardParameters = command.Split(' ');
+            var height = int.Parse(boardParameters[1]);
+            var width = int.Parse(boardParameters[0]);
+
+            return new Board(height, width);
+        }
+    }
+}
---
+++ Robots/RobotsTests/InputManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Robots;
+
+
+namespace RobotsTests
+{
+    [TestFixture]
+    class InputManagerTests
+    {
+        [TestCase("5 5","2 2 N","FF", "2 4 N")]
+        [TestCase("5 5","0 0 N","FRF", "1 1 E")]
+        [TestCase("5 5","0 0 E","FF", "2 0 E")]
+        [TestCase("5 5","2 2 N","FFRFFRFF", "4 2 S")]
+        public void Run_WhenOneRobotPostionAndRobotMovementProvided_RobotEndsInExpectedPlace(
+            string firstLine, string secondLine, string thirdLine, string expectedPosition)
+        {
+            var im = new InputManager();
+            var commands = new List<string> {firstLine, secondLine, thirdLine};
+
+            im.Run(commands);
+
+            Assert.AreEqual(expectedPosition, im.Report());
+        }
+    }
+}
