a1ca75220ba775f6db57b34aa0823feeff9e017c
Mateusz Kopij <sagasu@gmail.com>
2016-06-06 13:53:37 +0100

    adding board class that keeps info of board size and allowed check if a given position is on a board.

---
+++ Robots/Robots/Board.cs
@@ -0,0 +1,20 @@
+﻿namespace Robots
+{
+    public class Board
+    {
+        private readonly int _boardHeight;
+        private readonly int _boardWidth;
+
+        public Board(int height, int width)
+        {
+            _boardWidth = width;
+            _boardHeight = height;
+        }
+
+        public bool IsValidPosition(Position position)
+        {
+            return position.X >= 0 && position.X < _boardWidth&&
+                   position.Y >= 0 && position.Y < _boardHeight;
+        }
+    }
+}
---
+++ Robots/RobotsTests/BoardTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Robots;
+
+namespace RobotsTests
+{
+    [TestFixture()]
+    class BoardTests
+    {
+        [TestCase(0,0,1,1)]
+        [TestCase(3,4,5,5)]
+        [TestCase(1,7,2,8)]
+        public void IsValidPosition_WhenPositionInsideBord_ThenPositionIsValid(int positionX, int positionY, int boardWidth, int boardHeight)
+        {
+            var board = new Board(boardHeight, boardWidth);
+
+            var isValidPosition = board.IsValidPosition(new Position(positionX, positionY));
+
+            Assert.True(isValidPosition);
+        }
+
+        [TestCase(1,0,1,1)]
+        [TestCase(0,1,1,1)]
+        [TestCase(3,3,2,2)]
+        [TestCase(-1,1,2,2)]
+        public void IsValidPosition_WhenPositionOutsideBord_ThenPositionIsInvalid(int positionX, int positionY, int boardWidth, int boardHeight)
+        {
+            var board = new Board(boardHeight, boardWidth);
+
+            var isValidPosition = board.IsValidPosition(new Position(positionX, positionY));
+
+            Assert.False(isValidPosition);
+        }
+    }
+}
